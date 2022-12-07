6d099bba47d192765acdc51123ace923a1ea18c9
Pete Gadomski <pete.gadomski@gmail.com>
2022-12-07 16:08:07 -0700

    feat: re-enable collection tests

--- src/stactools/fws_nwi/commands.py
+++ src/stactools/fws_nwi/commands.py
@@ -1,11 +1,12 @@
 import logging
 import os
 import pathlib
-from typing import List, Optional
+from typing import List
 
 import click
 import requests
 from click import Command, Group, Path
+from pystac import CatalogType
 from tqdm import tqdm
 
 from stactools.fws_nwi import stac
@@ -29,46 +30,17 @@ def create_fwsnwi_command(cli: Group) -> Command:
         short_help="Creates a STAC collection",
     )
     @click.argument("destination")
-    @click.option(
-        "--id",
-        default="fws-nwi",
-        help="A custom collection ID, defaults to 'fws-nwi'",
-    )
-    @click.option(
-        "--thumbnail",
-        default="",
-        help="URL for the PNG or JPEG collection thumbnail asset (none if empty)",
-    )
-    @click.option(
-        "--nogeoparquet",
-        default=False,
-        help="Does not include the geoparquet-related metadata in the collection if set to `TRUE`.",
-    )
-    @click.option(
-        "--noshp",
-        default=False,
-        help="Does not include the shapefile-related metadata in the collection if set to `TRUE`.",
-    )
-    @click.option(
-        "--start_time",
-        default=None,
-        help="The start timestamp for the temporal extent, defaults to now. "
-        "Timestamps consist of a date and time in UTC and must be follow RFC 3339, section 5.6.",
-    )
     def create_collection_command(
         destination: str,
-        id: str = "fws-nwi",
-        thumbnail: str = "",
-        nogeoparquet: bool = False,
-        noshp: bool = False,
-        start_time: Optional[str] = None,
     ) -> None:
         """Creates a STAC Collection
 
         Args:
             destination (str): An HREF for the Collection JSON
         """
-        raise NotImplementedError
+        collection = stac.create_collection()
+        collection.set_self_href(destination)
+        collection.save(catalog_type=CatalogType.SELF_CONTAINED)
 
     @fwsnwi.command("create-item", short_help="Create a STAC item")
     @click.argument("source")
